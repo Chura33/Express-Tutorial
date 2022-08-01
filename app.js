b042603372170a2bb2824008cbd10b03a5e447f6
Chura <churabrady@gmail.com>
2022-08-01 13:43:11 -0700

    App js put delete post methods

--- app.js
+++ app.js
@@ -1,64 +1,80 @@
 const express = require("express")
 const app =express()
-const {products} = require('./data')
+let {people} = require('./data')
 
-app.get('/',(req,res)=>{
-  res.send('<h1>Home Page</h1> <a href="/api/products">Products</a>')
+// static assets
+app.use(express.static('./methods-public'))
+
+// parse form data
+app.use(express.urlencoded({extended:false}))
+app.get('/api/people',(req,res)=>{
+  res.status(200).json({success:true,data:people})
+})
+// parse json
+app.use(express.json());
+app.post('/api/people',(req,res)=>{
+  console.log(req.url);
+  const {name} = req.body;
+  if(!name){
+    return res.status(400).json({success:false,msg:"please provide name value"})
+  }
+  res.status(201).json({success:true,person:name})
 })
 
-app.get('/api/products',(req,res)=>{
-  const newProducts = products.map((product)=>{
-    const {id, name, image} = product
-    return {id,name,image}
+app.post("/api/postman/people",(req,res)=>{
+  const {name} = req.body
+  if(!name){
+    return res
+        .status(400)
+        .json({success:false, msg:"please provid name value"})
+  }
+  res.status(201).send({success:true,data:[...people, {id:8,name:name}]})
+})
+
+app.put('/api/people/:id',(req,res)=>{
+  const {id} = req.params;
+  const {name} = req.body;
+  const person = people.find((person)=>person.id === Number(id))
+
+  if(!person){
+    return res
+        .status(404)
+        .json({success:false, msg:`no person with ${id}`})
+  }
+
+  const newPeople = people.map((person)=>{
+    if(person.id === Number(id)){
+      person.name = name
+    }
+    return person
   })
-  res.json(newProducts)
+  res.status(200).json({success:true, data:newPeople})
 })
 
-// using route parameters
-app.get('/api/products/:productID',(req,res)=>{
-  // console.log(req)
-  // console.log(req.params)
-  const {productID} = req.params;
-  const singleProduct = products.find(product=>product.id ===Number(productID))
-  // console.log(productID)
-  if(!singleProduct){
-    return res.status(404).send('Product does not exist')
+app.delete('/api/people/:id',(req,res)=>{
+  const {id} = req.params;
+  const person = people.find(person=>person.id === Number(id));
+  if(!person){
+    return res
+        .status(404)
+        .json({success:false, msg:`no person with id ${id}`} )
   }
-  return res.json(singleProduct)
+  const newPeople = people.filter((person)=>{
+    return person.id !== Number(id)
+  })
+  res.status(200).json({success:true,data:newPeople})
 })
 
-// A slight mimic of a login process using req.params
-app.get('/api/products/:name/reviews/:id',(req,res)=>{
-  const {name,id} = req.params;
-  if (name === "Chura" && id==='123'){
-    return res.send('hello world')
+app.post('/login',(req,res)=>{
+
+  const {name} = req.body;
+  if(name){
+    return res.status(200).send(`Welcome ${name}`)
   }
-  else{
-    return res.send("Access denied")
-  }
+  res.status(401).send('Please provide credentials')
 })
 
-app.get('/api/v1/query',(req,res)=>{
-  const {search,limit} = req.query;
-  let sortedProducts = [...products];
-
-  if(search){
-    sortedProducts = sortedProducts.filter((product)=>{
-      return product.name.startsWith(search);
-    })
-  }
-
-  if(limit){
-    sortedProducts = sortedProducts.slice(0,Number(limit))
-  }
-  if (sortedProducts.length < 1){
-   
-   // res.status(200).send('no products matched your search')
-   return res.status(200).json({success:true,data:[]})
-  }
-  return res.status(200).json(sortedProducts)
-})
 
 app.listen(5000,()=>{
   console.log("This server is listening at port 5000....")
-})
+}) 
